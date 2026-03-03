7462bc53e266d2e048daaa59a392aa56e27a8535
fahrurozi16 <muhammadfahrurozi518@gmail.com>
2026-03-03 10:01:48 +0700

    update api list group,list client

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Semua route di file ini otomatis berprefix: /api
+ * Contoh URL lengkap: GET /api/list/client , GET /api/list/group
+ * (Base URL tergantung server, misal: http://localhost/atrindon_report/public atau http://atrindon_report.test)
+ */
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,8 +14,12 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\MyClientController;
 use App\Http\Controllers\GroupUserController;
 use App\Http\Controllers\ClientUserController;
+use App\Http\Controllers\ApiDocController;
 
 Route::post('/login', [AuthController::class, 'login']);
+
+// Dokumentasi API untuk frontend (tanpa auth agar bisa dilihat daftar API & params)
+Route::get('/docs', [ApiDocController::class, 'index']);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -38,7 +47,18 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/groupUser/edit/{id}', [GroupUserController::class, 'update']);
 
 
-    // Client List
+    // Client List - CRUD
     Route::get('/list/client', [ClientController::class, 'index']);
+    Route::get('/list/client/{id}', [ClientController::class, 'show']);
+    Route::post('/list/client', [ClientController::class, 'store']);
+    Route::put('/list/client/{id}', [ClientController::class, 'update']);
+    Route::delete('/list/client/{id}', [ClientController::class, 'destroy']);
+
+    // Group List - CRUD
     Route::get('/list/group', [GroupController::class, 'index']);
+    Route::get('/list/group/{id}', [GroupController::class, 'show']);
+    Route::post('/list/group', [GroupController::class, 'store']);
+    Route::put('/list/group/{id}', [GroupController::class, 'update']);
+    Route::delete('/list/group/{id}', [GroupController::class, 'destroy']);
+    
 });
